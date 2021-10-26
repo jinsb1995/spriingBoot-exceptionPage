b08c9f2a0d13717eaafa849c46b2fa76221c5a38
jinsb1995 <jinsb1995@gmail.com>
2021-10-27 00:06:40 +0900

    filter, interceptor, spring boot error-page

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,10 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+// 이거 주석 처리하면 ErrorPage가 자동으로 등록되어서 모든 오류는 /error를 호출하게 된다.
+// ErrorMvcAutoConfiguration이라는 클래스가 오류 페이지를 자동으로 등록하는 역할을 한다.
+// basicErrorController는 /error를 처리하기 위한 컨트롤러이다.
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
